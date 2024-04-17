af8da785893be1669dd47cf421561f71839040e1
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-04-17 10:33:20 -0700

    AirBnBmysql

--- console.py
+++ console.py
@@ -151,7 +151,6 @@ class HBNBCommand(cmd.Cmd):
             new_instance = HBNBCommand.classes[args_lst[0]](**kwrgs)
             new_instance.save()
             print(new_instance.id)
-            storage.save()
 
     def help_create(self):
         """ Help information for the create method """
