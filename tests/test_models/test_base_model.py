af5e4e1af3859d53d63058d5b1faabda4e9dfe80
ezzaytouni othmane <ezzaytouniothmane@gmail.com>
2024-04-17 11:18:37 -0700

    AirBnBmysql

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -97,3 +97,83 @@ class test_basemodel(unittest.TestCase):
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+    
+    def test_uuid(self):
+        """Test for uuid
+        """
+        inst1 = BaseModel()
+        inst2 = BaseModel()
+        inst3 = BaseModel()
+        list_insts = [inst1, inst2,
+                          inst3]
+        for inst in list_insts:
+            ins_uuid = inst.id
+            with self.subTest(uuid=ins_uuid):
+                self.assertIs(type(ins_uuid), str)
+        self.assertNotEqual(inst1.id, inst2.id)
+        self.assertNotEqual(inst1.id, inst3.id)
+        self.assertNotEqual(inst2.id, inst3.id)
+
+    def test_str_methd(self):
+        """Testing returns STR method"""
+        inst6 = BaseModel()
+        string_output = "[BaseModel] ({}) {}".format(inst6.id,
+                                                     inst6.__dict__)
+        self.assertEqual(string_output, str(inst6))
+
+
+class TestBaseModel(unittest.TestCase):
+    """this will test the basemodel"""
+
+    @classmethod
+    def setUpClass(cls):
+        """setup test"""
+        cls.base = BaseModel()
+        cls.base.name = "Kev"
+        cls.base.num = 20
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.base
+
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_check_for_doc_string_Base_Model(self):
+        """checking for doc strings"""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_methd_Base_Model(self):
+        """chekcing if Base model methds"""
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+    def test_init_Base_Model(self):
+        """test if the base is a type BaseModel"""
+        self.assertTrue(isinstance(self.base, BaseModel))
+
+    def test_save_BaesModel(self):
+        """test the save methd"""
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+
+    def test_to_dict_Base_Model(self):
+        """test if dictionary works"""
+        base_dict = self.base.to_dict()
+        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(base_dict['created_at'], str)
+        self.assertIsInstance(base_dict['updated_at'], str)
+
+
+if __name__ == "__main__":
+    unittest.main()
